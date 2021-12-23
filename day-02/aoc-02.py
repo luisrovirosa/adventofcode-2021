862c2335f1c6616eb6d9196be3614c25d6ead499
Luis Rovirosa Mairlot <luis@codium.team>
2021-12-23 18:33:46 +0100

    [day 2] part 2

--- day-02/aoc-02.py
+++ day-02/aoc-02.py
@@ -3,14 +3,16 @@ import functools
 lines = open('input.txt').read().split('\n')
 orders = [line.split(' ') for line in lines]
 
+
 def movement(command, units, position):
     operations = {
-        'forward': lambda: [position[0] + units, position[1]],
-        'up': lambda: [position[0], position[1]-units],
-        'down': lambda: [position[0], position[1] + units],
+        'forward': lambda: [position[0] + units, position[1] + position[2] * units, position[2]],
+        'up': lambda: [position[0], position[1], position[2] - units],
+        'down': lambda: [position[0], position[1], position[2] + units],
     }
     return operations[command]()
 
-x,y = functools.reduce(lambda position, order: movement(order[0], int(order[1]), position), orders, [0,0])
 
-print(x*y)
+x, y, aim = functools.reduce(lambda position, order: movement(order[0], int(order[1]), position), orders, [0, 0, 0])
+
+print(x * y)
