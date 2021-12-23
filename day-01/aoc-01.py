500bcbbb1b57533525fbf1d106d321ce1f0d48b0
Luis Rovirosa Mairlot <luis@codium.team>
2021-12-23 17:49:40 +0100

    [day 1] extract method

--- day-01/aoc-01.py
+++ day-01/aoc-01.py
@@ -1,4 +1,9 @@
 measures = open('input.txt').read().split('\n')
-res = [int(sub1) < int(sub2) for sub1, sub2 in zip(measures, measures[1:])]
-number_of_greater_elements = sum(is_greater for is_greater in res)
-print("First answer", number_of_greater_elements)
+
+
+def number_of_greater_elements(measures):
+    res = [int(sub1) < int(sub2) for sub1, sub2 in zip(measures, measures[1:])]
+    return sum(is_greater for is_greater in res)
+
+
+print("First answer", number_of_greater_elements(measures))
