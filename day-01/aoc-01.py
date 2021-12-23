a6f9d4101f1fbee6842327f046ed57ee83ca01f1
Luis Rovirosa Mairlot <luis@codium.team>
2021-12-23 17:51:23 +0100

    [day 1] part 2

--- day-01/aoc-01.py
+++ day-01/aoc-01.py
@@ -1,9 +1,10 @@
 measures = open('input.txt').read().split('\n')
 
-
 def number_of_greater_elements(measures):
     res = [int(sub1) < int(sub2) for sub1, sub2 in zip(measures, measures[1:])]
     return sum(is_greater for is_greater in res)
 
-
 print("First answer", number_of_greater_elements(measures))
+
+sum_three_elements =  [int(sub1) + int(sub2) + int(sub3) for sub1, sub2, sub3 in zip(measures, measures[1:], measures[2:])]
+print("Second answer", number_of_greater_elements(sum_three_elements))
