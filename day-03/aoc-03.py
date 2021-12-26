490c1c813dde44761d42344d294d3e763021185e
Luis Rovirosa Mairlot <luis@codium.team>
2021-12-26 21:15:51 +0100

    [day 3] part 2

--- day-03/aoc-03.py
+++ day-03/aoc-03.py
@@ -9,7 +9,8 @@ def digits(bits: list, criteria: callable):
     return [criteria(sum) for sum in sum_digit]
 
 def to_decimal(bits: list):
-    return int(''.join(bits), 2)
+    bits_as_string = [str(num) for num in bits]
+    return int(''.join(bits_as_string), 2)
 
 def gamma_digits(bits: list):
     return digits(bits, lambda sum: '1' if sum >= len(bits) / 2 else '0')
@@ -23,4 +24,17 @@ print('Gamma_rate', gamma_rate)
 epsilon_rate = to_decimal(epsilon_digits(bits))
 print('Epsilon_rate', epsilon_rate)
 
+def oxigen_generation_rating(bits: list, function: callable, position: int):
+    digit = int(function(bits)[position])
+    remaining_bits = list(filter(lambda bit: bit[position] == digit, bits))
+    if (len(remaining_bits) > 1):
+        return oxigen_generation_rating(remaining_bits, function, position+1)
+    return remaining_bits[0]
+
 print('Power consumption', gamma_rate*epsilon_rate)
+
+oxygen = to_decimal(oxigen_generation_rating(bits, gamma_digits, 0))
+print('Oxigen', oxygen)
+co2 = to_decimal(oxigen_generation_rating(bits, epsilon_digits, 0))
+print('CO2', co2)
+print('Life support', oxygen * co2)
