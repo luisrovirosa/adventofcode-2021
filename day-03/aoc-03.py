ce86d376b80b2315b7188a6927bbe6351a664891
Luis Rovirosa Mairlot <luis@codium.team>
2021-12-26 20:56:13 +0100

    [day 3] preparatory refactoring

--- day-03/aoc-03.py
+++ day-03/aoc-03.py
@@ -3,14 +3,24 @@ import numpy as np
 lines = open('input.txt').read().split('\n')
 bits = [[int(char) for char in line] for line in lines]
 
-digits_array = np.array(bits).T
+def digits(bits: list, criteria: callable):
+    digits_array = np.array(bits).T
+    sum_digit = [sum(digit_array) for digit_array in digits_array]
+    return [criteria(sum) for sum in sum_digit]
 
-sum_digit = [sum(digit_array) for digit_array in digits_array]
-gamma_digits = ['1' if sum > len(lines) / 2 else '0' for sum in sum_digit]
-gamma_rate = int(''.join(gamma_digits), 2)
-epsilon_digits = ['0' if sum > len(lines) / 2 else '1' for sum in sum_digit]
-epsilon_rate = int(''.join(epsilon_digits), 2)
+def to_decimal(bits: list):
+    return int(''.join(bits), 2)
 
+def gamma_digits(bits: list):
+    return digits(bits, lambda sum: '1' if sum >= len(bits) / 2 else '0')
+
+def epsilon_digits(bits: list):
+    return digits(bits, lambda sum: '0' if sum >= len(bits) / 2 else '1')
+
+gamma_rate = to_decimal(gamma_digits(bits))
 print('Gamma_rate', gamma_rate)
+
+epsilon_rate = to_decimal(epsilon_digits(bits))
 print('Epsilon_rate', epsilon_rate)
+
 print('Power consumption', gamma_rate*epsilon_rate)
